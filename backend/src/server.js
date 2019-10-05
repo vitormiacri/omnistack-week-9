d4506fce45f83bc32c2bd458b7fbd6083a662e7e
Vitor Morelli Miacri <--global>
2019-10-04 21:48:55 -0300

    Backend fixes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const routes = require("./routes");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -12,7 +14,9 @@ mongoose.connect(
   }
 );
 
+app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 app.listen(3333);
